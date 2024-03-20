d6d5a5ca1c667e1f149c31c51da3596f3d00857e
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-20 06:39:24 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,8 @@
+// let array: (string | number) [] = ['Zaid','Huzaifa',3,5,8,'aqib','khizer']
+// console.log(array[6])
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3]) 
+// console.log(array[4])
+// console.log(array[5])
